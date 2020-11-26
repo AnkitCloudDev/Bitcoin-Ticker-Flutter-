a46428d60e0735218051e7c05dae01503aeba367
AnkitCloudDev <ankitdongre96@gmail.com>
2020-11-26 14:55:40 +0000

    First Commit

---
+++ lib/coin_data.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+const kApi_key="?apikey=99770410-5E5F-4643-8F7D-929DAF71CA5E";
+// TODO: GOTO https://www.coinapi.io/Pricing, scroll down and click get a free API key. Create an account and you will get a free API key mailed to you,
+const kurl="https://rest.coinapi.io/v1/exchangerate/";
+
+
+//List of all the crypto's we want the rates for
+const List<String> crpytoList=[
+  'BTC',
+  'ETH',
+  'LTC',
+];
+//List of Currencies to Choose from
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+class CoinData {
+
+  Future<double> getCoinData(String currency) async{
+    String url = kurl + 'BTC/' + currency + kApi_key;
+    //Building the REST API String by adding the coin Detail and API key
+    print(url);
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      var exchangeData = jsonDecode(response.body);
+      print(exchangeData['rate']);
+      return exchangeData['rate'];
+    }
+    else {
+      print("Sorry!! Something went wrong");
+    }
+
+  }
+  Future<double> getLTCData(String currency) async {
+    String url = kurl + 'LTC/' + currency + kApi_key;
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      var exchangeData = jsonDecode(response.body);
+      print(exchangeData['rate']);
+      return exchangeData['rate'];
+    }
+    else {
+      print("Sorry!! Something went wrong");
+    }
+  }
+    Future<double> getETHData(String currency) async{
+      String url=kurl+'ETH/'+currency+kApi_key;
+      http.Response response=await http.get(url) ;
+      if(response.statusCode==200){
+        var exchangeData=jsonDecode(response.body);
+        print(exchangeData['rate']);
+        return exchangeData['rate'];
+      }
+      else{
+        print("Sorry!! Something went wrong");
+
+      }
+
+  }
+
+}
---
+++ lib/price_screen.dart
@@ -0,0 +1,155 @@
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'dart:io' as pt show Platform ;
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  var selectedValue="USD";
+  double rate;
+
+  List<String> printedRates=['?','?','?'];
+  Map<String,double> rates={};
+CoinData coinData=CoinData();
+  List<Widget> list;
+  String dropdownValue;
+    List<Widget> getDropDownItems(){
+      list=List<DropdownMenuItem<String>>();
+      for(String i in currenciesList){
+        list.add(buildDropdownMenuItem(i));
+      }
+      return list;
+    }
+  DropdownButton<String> getDropDownButton(){
+      return DropdownButton<String>(
+          items: getDropDownItems(
+
+          )
+      ,
+      value: selectedValue,
+      onChanged: (value) async{
+
+        // printedRate=
+
+        rates['ETH']=await coinData.getETHData(value);
+        rates['LTC']=await coinData.getLTCData(value);
+        rates['BTC']=await coinData.getCoinData(value);
+        printedRates[0]=rates['BTC'].toStringAsFixed(3);
+        printedRates[1]=rates['ETH'].toStringAsFixed(3);
+        printedRates[2]=rates['LTC'].toStringAsFixed(3);
+        print(printedRates[0]);
+            setState(()  {
+
+              selectedValue=value;
+            });
+      print(value);
+      },
+
+      );
+    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+
+          CardWidget(printedRate: printedRates[0], selectedValue: selectedValue,crypto: 'BTC',),
+          CardWidget(printedRate: printedRates[1], selectedValue: selectedValue,crypto: 'ETH',),
+          CardWidget(printedRate: printedRates[2], selectedValue: selectedValue,crypto: 'LTC',),
+
+          Container(
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: getPicker(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  CupertinoPicker buildIosPicker() {
+    return CupertinoPicker(
+            itemExtent: 32.0 ,
+            onSelectedItemChanged: (index)async {
+
+              setState(()  {
+                selectedValue=list.getRange(index, index+1) as String;
+
+              });
+              rates['BTC']=await coinData.getCoinData(selectedValue);
+              rates['ETH']=await coinData.getETHData(selectedValue);
+              rates['LTC']=await coinData.getLTCData(selectedValue);
+
+              printedRates[0]=rates['BTH'].toString();
+              printedRates[1]=rates['ETH'].toString();
+              printedRates[2]=rates['LTC'].toString();
+            },
+            children: getDropDownItems(),
+
+          );
+  }
+
+  DropdownMenuItem<String> buildDropdownMenuItem(String t) => DropdownMenuItem(child: Text(t, style: TextStyle(fontSize: 20.0,color: Colors.white,letterSpacing: 2),),value:t ,);
+
+  Widget getPicker() {
+
+    if(pt.Platform.isIOS){
+      return buildIosPicker();
+    }
+    else if(pt.Platform.isAndroid){
+      return getDropDownButton();
+    }
+
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({
+    Key key,
+    @required this.printedRate,
+    @required this.selectedValue,
+    @required this.crypto,
+  }) : super(key: key);
+
+  final String printedRate;
+  final String selectedValue;
+  final String crypto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $crypto = $printedRate $selectedValue',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
